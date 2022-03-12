2cd4fb7bad9738ce7a17f6de1a2498f977badd8e
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-03-12 13:17:08 -0700

    added content highlight based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,91 @@
 console.log("HELLO");
 
 // variable list
-var today = moment().format("dddd MMMM Do, YYYY");
+var today = moment().format("LLLL");
 var timeSlot = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
-
-var timeAsOfNow = moment().format("HH");
 
 // setting current date 
 $("#currentDay").text(today);
 
+var now = new Date().getHours();
 
+function colorCode() {
+  if (now > 9) {
+    $("#9am").addClass("past");
+  } else if (now >= 9 && now < 10) {
+    $("#9am").addClass("present");
+  } else if (now < 9) {
+    $("#9am").addClass("future");
+  }
+
+  if (now > 10) {
+    $("#10am").addClass("past");
+  } else if (now >= 10 && now < 11) {
+    $("#10am").addClass("present");
+  } else if (now < 10) {
+    $("#10am").addClass("future");
+  }
+
+  if (now > 11) {
+    $("#11am").addClass("past");
+  } else if (now >= 11 && now < 12) {
+    $("#11am").addClass("present");
+  } else if (now < 11) {
+    $("#11am").addClass("future");
+  }
+
+  if (now > 12) {
+    $("#12pm").addClass("past");
+  } else if (now >= 12 && now < 13) {
+    $("#12pm").addClass("present");
+  } else if (now < 12) {
+    $("#12pm").addClass("future");
+  }
+
+  if (now > 13) {
+    $("#1pm").addClass("past");
+  } else if (now >= 13 && now < 14) {
+    $("#1pm").addClass("present");
+  } else if (now < 13) {
+    $("#1pm").addClass("future");
+  }
+
+  if (now > 14) {
+    $("#2pm").addClass("past");
+  } else if (now >= 14 && now < 15) {
+    $("#2pm").addClass("present");
+  } else if (now < 14) {
+    $("#2pm").addClass("future");
+  }
+
+  if (now > 15) {
+    $("#3pm").addClass("past");
+  } else if (now >= 15 && now < 16) {
+    $("#3pm").addClass("present");
+  } else if (now < 15) {
+    $("#3pm").addClass("future");
+  }
+
+  if (now > 16) {
+    $("#4pm").addClass("past");
+  } else if (now >= 16 && now < 17) {
+    $("#4pm").addClass("present");
+  } else if (now < 16) {
+    $("#4pm").addClass("future");
+  }
+
+  if (now > 17) {
+    $("#5pm").addClass("past");
+  } else if (now >= 17 && now < 18) {
+    $("#5pm").addClass("present");
+  } else if (now < 17) {
+    $("#5pm").addClass("future");
+  }
+}
+
+colorCode();
+
+// Save list to local storage on click
 $(".9amBtn").on("click", function() {
   var lsText = $(".9am").val();
   localStorage.setItem("09:00", lsText);
@@ -56,6 +132,7 @@ $(".5pmBtn").on("click", function() {
   localStorage.setItem("17:00", lsText);
 });
 
+// Get List from local Storage
 $(".9am").val(localStorage.getItem("09:00"));
 $(".10am").val(localStorage.getItem("10:00"));
 $(".11am").val(localStorage.getItem("11:00"));
@@ -65,3 +142,9 @@ $(".2pm").val(localStorage.getItem("14:00"));
 $(".3pm").val(localStorage.getItem("15:00"));
 $(".4pm").val(localStorage.getItem("16:00"));
 $(".5pm").val(localStorage.getItem("17:00"));
+
+// Delete item from row and local storage
+$("#9amBtnDel").on("click", function(){
+  var lsText = $(this).siblings(".9am").val("");
+  localStorage.removeItem("09:00", lsText)
+});
