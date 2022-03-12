05a8fd09fb77ef0b3f6964e0984b922892d784e6
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-03-11 21:43:25 -0700

    added task rows and set currentDay time

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+console.log("HELLO");
+
+var today = moment().format("LLLL");
+var timeSlot = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+
+//Setting current Date in Jumbotron
+$("#currentDay").text(today);
+
